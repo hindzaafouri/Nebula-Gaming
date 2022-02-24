77cb6b98dfd6e384f6191c13722d5b8e88b8302e
DroidZed <aymen.dragon5@gmail.com>
2022-02-24 01:25:22 +0100

    changed desktop project and removed web and mobile projects

--- desktop/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module tn.nebulagaming.desktop {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens tn.nebulagaming.desktop to javafx.fxml;
-    exports tn.nebulagaming.desktop;
-}
-*/
